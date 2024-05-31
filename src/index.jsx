5f50528d4e4d9660bf70b7b83a98dde92fe77a7c
MiroMuro <miro.laukkanen44@gmail.com>
2024-05-31 19:57:49 +0300

    Added cypress tests & steps

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
+/* eslint-env browser */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.createRoot(ReactDOM.document.getElementById("app")).render(
+ReactDOM.createRoot(document.getElementById("app")).render(
   <Router>
     <App />
   </Router>
