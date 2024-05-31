7b01f166f000cc28a5144d509447da9fd97a16c3
MiroMuro <miro.laukkanen44@gmail.com>
2024-05-31 20:43:17 +0300

    added eslint cypress plugin

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // parameters: on, config
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
